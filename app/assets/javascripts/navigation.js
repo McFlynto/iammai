37732325d6cff9089ba32ce585350553575b8e3c
Julian Treutler <julian.treutler@gmail.com>
2018-05-18 23:22:23 +0200

    Styles mobile navigation and project grid

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
   $('#nav-toggle').on('click', () => {
     $('#navigation').addClass('navigation__show');
-  })
+  });
+  $('#close-nav').on('click', () => {
+    $('#navigation').removeClass('navigation__show');
+  });
 })
