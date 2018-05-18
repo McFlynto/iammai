2fd592bd594ccceb16d4b5ca24d8aeb6c4d065a9
Julian Treutler <julian.treutler@gmail.com>
2018-05-18 23:54:07 +0200

    Slick slider moves on click

--- app/assets/javascripts/initalize.js
+++ app/assets/javascripts/initalize.js
@@ -1,5 +1,13 @@
 $(document).ready(() => {
-  $('.scroller').slick({
-    dots: true
-  })
+  const $scroller = $('.scroller').slick({
+    dots: true,
+    arrows: false,
+    adaptiveHeight: true,
+    focusOnSelect: true,
+    fade: true
+  });
+
+  $scroller.find('.slick-slide').on('click', () => {
+    $scroller.slick('slickNext');
+  });
 })
